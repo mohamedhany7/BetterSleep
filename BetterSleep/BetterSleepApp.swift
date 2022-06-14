4fa41365396147fb703fc67b4dbeb4cf765abd9d
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-06-15 01:58:22 +0200

    Initial Commit

---
+++ BetterSleep/BetterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepApp.swift
+//  BetterSleep
+//
+//  Created by Mohamed Hany on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
