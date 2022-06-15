977aef208f02ebaec6c08ec44cf0ddd8f292a62e
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-06-15 20:24:59 +0200

    third commit

--- BetterSleep/ContentView.swift
+++ BetterSleep/ContentView.swift
@@ -11,9 +11,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var selectedCoffeeAmount = 1
+    private var coffeeAmount = Array(1...10)
     
-    @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
@@ -26,33 +26,38 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            Form {
-                Section{
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+            VStack{
+                Form {
+                    Section{
+                        DatePicker("Will wake up at:", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            //.labelsHidden()
+                            .font(.headline)
+                    }
+                    
+                    Section{
+                        Text("Desired amount of sleeping hours")
+                            .font(.headline)
 
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                }
-                
-                Section{
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                }
-                
-                Section{
-                    Text("Daily coffee intake")
-                        .font(.headline)
-
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                }
-                
-                .alert(alertTitle, isPresented: $showingAlert) {
-                    Button("OK") { }
-                } message: {
-                    Text(alertMessage)
+                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    }
+                    
+                    Section{
+                        Picker("Daily coffee intake", selection: $selectedCoffeeAmount) {
+                            ForEach(coffeeAmount, id: \.self) { amount in
+                                Text(String(amount))
+                            }
+                        }
+                    }
+                    
+                    Section{
+                        Text("Your ideal bedtime is…")
+                            .font(.headline)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        Text(alertMessage)
+                            .font(.title.bold())
+                            .foregroundColor(.blue)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                    }
                 }
             }
             .navigationTitle("BetterRest")
@@ -71,14 +76,11 @@ struct ContentView: View {
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(selectedCoffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "Your ideal bedtime is…"
             alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
         showingAlert = true
